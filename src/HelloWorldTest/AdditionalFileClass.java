c0f2d4d12aa7f0085ba63e35d8a19ef347e59405
irvinmags <hector.punzalan@afpayments.com>
2016-05-03 14:07:50 +0800

    Created and added a class.

---
+++ src/HelloWorldTest/AdditionalFileClass.java
@@ -0,0 +1,9 @@
+package HelloWorldTest;
+
+public class AdditionalFileClass {
+	
+	public void addString(){
+		System.out.println("Adding a new class");
+	}
+
+}
--- src/HelloWorldTest/TestForGitHub.java
+++ src/HelloWorldTest/TestForGitHub.java
@@ -6,8 +6,11 @@ public class TestForGitHub {
 	
 	public static void main(String[] args)
 	{
-	
+		AdditionalFileClass addFile = new AdditionalFileClass();
 		System.out.println("Hector Irvin Punzalan, 03/05/2016 - HELLO WORLD");
+		System.out.println("Comparing new changes to the original");
+		addFile.addString();
+		
 	}
 
 }
