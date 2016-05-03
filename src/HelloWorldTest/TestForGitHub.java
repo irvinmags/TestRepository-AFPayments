8730c4713e450f111288f5d372d73d83ffda61af
irvinmags <hector.punzalan@afpayments.com>
2016-05-03 14:23:23 +0800

    Just added some lines

--- src/HelloWorldTest/TestForGitHub.java
+++ src/HelloWorldTest/TestForGitHub.java
@@ -6,11 +6,8 @@ public class TestForGitHub {
 	
 	public static void main(String[] args)
 	{
-		
 		System.out.println("Hector Irvin Punzalan, 03/05/2016 - HELLO WORLD");
 		System.out.println("Comparing new changes to the original");
-	
-		
 	}
 
 }
