2b0372bcddd9c80076512299553cb51fe61058a2
irvinmags <hector.punzalan@afpayments.com>
2016-05-03 09:49:24 +0800

    Added HelloWorld+2

--- src/HelloWorldTest/TestForGitHub.java
+++ src/HelloWorldTest/TestForGitHub.java
@@ -3,8 +3,11 @@ package HelloWorldTest;
 public class TestForGitHub {
 	
 	
+	
 	public static void main(String[] args)
 	{
+		AdditionalClass hello = new AdditionalClass();
+		hello.passString();
 		System.out.println("Test Lang");
 	}
 
