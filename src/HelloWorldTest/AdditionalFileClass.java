d64e36c317be4945d0cff637ca93fa003908d74c
irvinmags <hector.punzalan@afpayments.com>
2016-05-03 14:12:14 +0800

    Removed Class

--- src/HelloWorldTest/AdditionalFileClass.java
+++
@@ -1,9 +0,0 @@
-package HelloWorldTest;
-
-public class AdditionalFileClass {
-	
-	public void addString(){
-		System.out.println("Adding a new class");
-	}
-
-}
--- src/HelloWorldTest/TestForGitHub.java
+++ src/HelloWorldTest/TestForGitHub.java
@@ -6,11 +6,10 @@ public class TestForGitHub {
 	
 	public static void main(String[] args)
 	{
-		AdditionalFileClass addFile = new AdditionalFileClass();
 		
 		System.out.println("Hector Irvin Punzalan, 03/05/2016 - HELLO WORLD");
 		System.out.println("Comparing new changes to the original");
-		addFile.addString();
+	
 		
 	}
 
